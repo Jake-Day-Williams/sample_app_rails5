cc441e1e9f07a7c3290f4c04605054850d38b5bb
Jake Day Williams <jake@wildacademy.com>
2016-07-21 13:21:23 -0600

    Add test for alert text returned

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -15,7 +15,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               password_confirmation: "bar" } }
 
     assert_template 'users/edit'
-    assert_select 'div.alert.alert-danger'
+    assert_select 'div.alert', "The form contains 4 errors."
   end
 
   test "successful edit" do
